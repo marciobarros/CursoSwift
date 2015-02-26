d056e254e569848a3cc02824aab5d5d8967c047c
Marcio Barros <marcio.barros@gmail.com>
2015-02-26 15:29:19 -0300

    Project PhysicsBottle

---
+++ Physics Bottle/Physics Bottle/GameScene.swift
@@ -0,0 +1,51 @@
+//
+//  GameScene.swift
+//  Physics Bottle
+//
+//  Created by Marcio Barros on 2/26/15.
+//  Copyright (c) 2015 UNIRIO. All rights reserved.
+//
+
+import SpriteKit
+
+extension Float {
+	static func range(min: CGFloat, max: CGFloat) -> CGFloat {
+		return CGFloat(Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+	}
+}
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!";
+        myLabel.fontSize = 65;
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
